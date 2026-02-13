82a265e4e06b407b9bef22e33ce2d53b95e0b90f
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-02-13 19:47:41 +0100

    Update config.h

--- promicro_handwired_4x6/config.h
+++ promicro_handwired_4x6/config.h
@@ -3,8 +3,10 @@
 // Debug (kann bleiben)
 #define DEBUG_MATRIX_SCAN_RATE
 #define DEBUG_MATRIX
+#define ENCODER_BTN_PIN B1  // <-- HIER deinen Pin eintragen (Beispiel!)
 
 // WS2812 / RGBLIGHT (QMK aktuell)
 #define WS2812_DI_PIN F4          // Arduino A3 = F4 (Pro Micro)
 #define RGBLIGHT_LED_COUNT 10     // Anzahl NeoPixel
 #define RGBLIGHT_LIMIT_VAL 80
+
