85a16950daba75fec378d5de6a9e74ee050f1792
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-02-13 19:39:20 +0100

    Update keymap.c

--- promicro_handwired_4x6/keymaps/default/keymap.c
+++ promicro_handwired_4x6/keymaps/default/keymap.c
@@ -3,12 +3,12 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_A, KC_B, KC_C, KC_ENT,
-        KC_1, KC_2, KC_3, KC_4,
-        KC_5, KC_6, KC_7, KC_8,
-        KC_9, KC_0, KC_DOT, KC_SPC,
-        KC_ESC, KC_TAB, KC_BSPC, KC_DEL,
-        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+        KC_NO, KC_F14, KC_F15, KC_BACKSLASH,
+        KC_NUM_LOCK, KC_KP_ASTERISK, KC_KP_SLASH, KC_KP_MINUS,
+        KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
+        KC_KP_4, KC_KP_5, KC_KP_6, KC_NO,
+        KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,
+        KC_KP_0, KC_NO, , KC_KP_DOT, KC_NO
     )
 };
 
@@ -21,3 +21,4 @@ void keyboard_post_init_user(void) {
     wait_ms(150);
     rgblight_sethsv_noeeprom(85, 255, 80);
 }
+
